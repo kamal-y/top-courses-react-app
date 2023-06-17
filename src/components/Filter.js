e0c58133b57822332dc094d9003ad557cd23c34f
kamal-y <kamal9967222@gmail.com>
2023-06-17 13:43:29 +0530

    Updated project with new changes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,9 +13,9 @@ const Filter = ({filterData,category,setCategory,}) => {
                     (data) => {
                         return (<button key={data.id} onClick={()=>categoryHandler(data.title)} className={`text-lg px-2 py-1 rounded-md font-medium
                                   text-white bg-black hover:bg-opacity-50 
-                                    border-2 bg-opacity-60 border-white transition-all
+                                    border-2 bg-opacity-60 transition-all
                                      duration-300
-                                     ${category === data.title ? "bg-opacity-60 border-white" : "bg-opacity-40 border-transparent"}`}>{data.title}</button>);
+                                     ${(category === data.title) ? "bg-opacity-60 border-white" : "bg-opacity-40 border-transparent"}`}>{data.title}</button>);
                     }
                 )
             }
